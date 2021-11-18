bf573027da066213daf65093bc4fd524457e8726
Ishita938313 <88820522+Ishita938313@users.noreply.github.com>
2021-11-18 14:55:11 +0530

    Delete ZJM-Tourist-Inn directory

--- ZJM-Tourist-Inn/src/main/java/com/cyan/hotel/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.cyan.hotel.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String resourceType, String name, String value){
-        super("Error: " + resourceType + " with " + name +
-                "[" + value + "] not found!!");
-    }
-}
